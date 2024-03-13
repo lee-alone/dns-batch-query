a28b59add38c731c80472061541b1702bb54b852
lee-alone <57851395+lee-alone@users.noreply.github.com>
2024-03-13 11:05:00 +0800

    Update main.bat

--- main.bat
+++ main.bat
@@ -2,6 +2,10 @@
 setlocal EnableDelayedExpansion
 
 set "filename=chinalist.txt"
+set "outputfile=resolved.txt"
+
+rem 删除已存在的 resolved.txt 文件
+if exist "%outputfile%" del "%outputfile%"
 
 for /f "usebackq delims=" %%a in ("%filename%") do (
     set "domain=%%a"
@@ -10,9 +14,13 @@ for /f "usebackq delims=" %%a in ("%filename%") do (
     nslookup !domain! >nul 2>nul
     if !errorlevel! equ 0 (
         echo !domain! resolved successfully.
+        echo !domain!>>"%outputfile%"
     ) else (
         echo Failed to resolve !domain!
     )
 )
+
+echo.
+echo Resolved domains are saved in %outputfile%.
 
 endlocal
